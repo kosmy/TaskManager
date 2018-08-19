81ec59ae2df50738bd1ef7b3e48d86574b48b9d2
Nikita Kostash <nikitaakostash@gmail.com>
2018-08-19 21:52:22 +0300

    model has been created

---
+++ TaskManager/TaskManagerModel.swift
@@ -0,0 +1,21 @@
+//
+//  TaskManagerModel.swift
+//  TaskManager
+//
+//  Created by Nikita Kostash on 19.08.2018.
+//  Copyright © 2018 Nikita Kostash. All rights reserved.
+//
+
+import UIKit
+
+class TaskManagerModel: NSObject {
+    
+    static var task = [String].init()
+    static var desc = [String].init()
+    
+    static func addTask(name: String, description: String) {
+        task.append(name)
+        task.append(description)
+    }
+
+}
